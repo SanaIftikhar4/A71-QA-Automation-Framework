3a720220c680bd7e2681d45e0fab9e3418dadc2e
Sana Iftikhar <161517854+SanaIftikhar4@users.noreply.github.com>
2025-05-27 16:15:31 -0500

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -16,9 +16,9 @@ public class LoginTests extends BaseTest {
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+    
         // TODO: Fix the URL if the test fails
-        String url = "htpps://qa.koel.app/";
+        String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
