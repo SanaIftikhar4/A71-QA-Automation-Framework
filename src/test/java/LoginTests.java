765af31b592d415f5d120773fba29f0d1e4da71e
Sana Iftikhar <161517854+SanaIftikhar4@users.noreply.github.com>
2025-05-31 13:57:09 -0500

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -6,21 +6,22 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
-    @Test
-    public void navigationToLoginPage() {
+    public class LoginTests extends BaseTest {
+        @Test
+        public void navigationToLoginPage() {
 
 //      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            WebDriver driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        // TODO: Fix the URL if the test fails
-        String url = "htpps://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+            // TODO: Fix the URL if the test fails
+            String url = "https://qa.koel.app/";
+            driver.get(url);
+            Assert.assertEquals(driver.getCurrentUrl(), url);
+            driver.quit();
+        }
     }
 }
