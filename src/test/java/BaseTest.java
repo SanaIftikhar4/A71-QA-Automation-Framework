7070ba5914df3a27db2f54204813480854c50081
Sana Iftikhar <161517854+SanaIftikhar4@users.noreply.github.com>
2025-05-31 15:38:37 -0500

    Refactor login framework: added BaseTest setup, created LoginPage POM, updated LoginTests with assertions
    Refactored login automation by:
    - Adding BaseTest with suite-level setup/teardown
    - Implementing LoginPage POM with locators and helper methods
    - Updating LoginTests to use LoginPage and include assertions
    - Improving test stability with explicit waits

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,37 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
 
+    protected WebDriver driver = null ;
+    protected String url = "https://qa.koel.app/";
+
     @BeforeSuite
-    static void setupClass() {
+    public void setupDriver() {
+
         WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");  // if needed
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.get(url);
+
     }
+
+
+    @AfterSuite
+    public void tearDown(){
+        if(driver!= null){
+            driver.quit();
+        }
+    }
+
 }
